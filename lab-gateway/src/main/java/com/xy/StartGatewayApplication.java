f1ef1a6e39ec0c6531141fb3c92a7927e8d9e11e
sunminx <2421500571@qq.com>
2022-04-17 22:58:37 +0800

    build gateway

---
+++ lab-gateway/src/main/java/com/xy/StartGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.xy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartGatewayApplication.class, args);
+    }
+}
