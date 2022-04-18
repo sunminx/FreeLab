eabbed107a5a0ada9bd3186ff62b3993f22ce98b
sunminx <2421500571@qq.com>
2022-04-18 22:20:20 +0800

    add swagger to manage api

--- lab-common/src/main/java/com/xy/entity/ResultEntity.java
+++ lab-common/src/main/java/com/xy/entity/ResultEntity.java
@@ -1,15 +1,23 @@
 package com.xy.entity;
 
 import com.xy.constant.Constant;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@ApiModel(value = "服务接口响应实体")
 @Data
 @NoArgsConstructor
 public class ResultEntity<T> {
-    private int code; // 状态码
-    private String message; //
-    private T data; // 数据
+    @ApiModelProperty("状态码")
+    private int code;
+
+    @ApiModelProperty("接口响应信息")
+    private String message;
+
+    @ApiModelProperty("接口响应数据")
+    private T data;
 
     public ResultEntity(T data) {
         this(Constant.SUCCESS_CODE, Constant.SUCCESS_MESSAGE, data);
